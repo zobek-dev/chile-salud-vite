992122a7a2b77cd885ad6b96d9e179d85a171523
sebastian veliz <zobek@MacBook-Air-de-sebastian.local>
2023-02-25 20:40:15 -0300

    sabado 25 febrero

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,28 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 window.addEventListener('DOMContentLoaded', ()=>{
   const links = document.querySelectorAll('.navbar-nav .nav-item .nav-link')
   const navBar = document.querySelector('#navbarSupportedContent') 
+  const header = document.querySelector('.navbar')
 
+  //aos init
+  AOS.init()
+
+  //close mobile nav when click
   links.forEach(el => {
     el.addEventListener('click', () => {
       navBar.classList.remove('show')
     })
   })
-  console.log(links)
+
+  //scroll sticky header
+  window.addEventListener('scroll', ()=>{
+    scrollY > 53 ? header.style.position = 'sticky' : header.style.position = 'relative'
+  })
+
+  //Farmacias adheridas
+  
 })
